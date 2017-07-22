f1bb0786598a97325de4ba95b70fbba178da5630
Invision Solutions <info@invisionsolutions.ca>
2017-07-22 22:57:36 +0500

    Documentation

--- ListFunctions.py
+++ ListFunctions.py
@@ -1,14 +1,14 @@
 class ListFunctions:
 	def __init__():
 		pass
-	def CookielistContains(x, listA):
+	def CookielistContains(self, x, listA):
         """
         :param x: Name in list
         :param: listA: List in which Name needs to be found
         :desc: This function places true value at index where name is found
         :return: list
         """
-	    def nameFinder(y):
+	    def nameFinder(self, y):
             """
             :param y: single iterator for map function
             :desc: Function used in map function to iterate through map function
